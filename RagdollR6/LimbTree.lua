e5ba165f1511e1a04ce6e2d1cc303ee92f7d984e
F4YKING <63247725+F4YKING@users.noreply.github.com>
2024-02-04 20:06:53 +0700

    Syntax

--- RagdollR6/LimbTree.lua
+++ RagdollR6/LimbTree.lua
@@ -1,12 +1,21 @@
+-- // F4YKING - GITHUB
+-- // 04 February 2024 - 19:28:22
+
+-- // LimbTree.lua
+
+--[[
+    This is just a list of R6 body part names.
+]]
+
 return {
 	"Head",
-	
+
 	"Torso",
 	"HumanoidRootPart",
-	
+
 	"Right Arm",
 	"Left Arm",
-	
+
 	"Right Leg",
 	"Left Leg",
 }
