4bf548ae382c94d313458137044df660eeef5f68
F4YKING <63247725+F4YKING@users.noreply.github.com>
2024-02-04 20:06:48 +0700

    init

--- RagdollR6/init.lua
+++ RagdollR6/init.lua
@@ -1,163 +1,70 @@
--- // F4YKING - Github
--- // 22 September 2023
+-- // F4YKING - GITHUB
+-- // 04 February 2024 - 19:05:24
 
---[[]]
+-- // init.lua - RagdollR6.lua
+
+--[[
+
+]]
 
 ---- Variables ----
--- Packages
+-- Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Trove
 local Trove = require(script.Parent.Trove)
 
 -- Modules
 local LimbTree = require(script.LimbTree)
-local AttachmentData = require(script.AttachmentData)
-local BallSocketData = require(script.BallSocketData)
 
 ---- Initialize ----
-
 -- RagdollR6
 local RagdollR6: RagdollR6 = {}; export type RagdollR6 = typeof(RagdollR6.new())
 RagdollR6.__index = RagdollR6
 
---[=[
-    @param plr Player
-    @return RagdollR6
-]=]
-
-function RagdollR6.new(plr: Player)
+-- New
+function RagdollR6.new(char: Model)
     local self = setmetatable({}, RagdollR6)
     self._trove = Trove.new()
 
-    self.Player = plr :: Player
+    self.Char = char :: Model
+    self.Humanoid = (char and char:FindFirstChildWhichIsA("Humanoid")) :: Humanoid?
 
-    self.Char = nil :: Model
-    self.Humanoid = nil :: Humanoid
+    self.HumanoidRootPart = (char and char:FindFirstChild("HumanoidRootPart")) :: Part?
 
-    self.Limbs = {}
     self.Atts = {}
-
     self.Accessories = {}
 
-    return self
+    self:Init(); return self
 end
 
---[=[
-    @param char Model
-]=]
+function RagdollR6:Init()
 
-function RagdollR6:Init(char: Model)
-
-	self.Char = char
-    self.Humanoid = char and char:FindFirstChild("Humanoid")
-
-    if not self:Checks() then self:Destroy() return end
-
+    if not self:Checks() then self:Stop(true); return end
     self.Humanoid.BreakJointsOnDeath = false
 
-    for _, LimbName in ipairs(LimbTree) do
-		self.Limbs[LimbName] = self.Char:FindFirstChild(LimbName)
-	end
-
 end
-
---[=[
-
-]=]
-
-function RagdollR6:Start()
-
-    self.Limbs.HumanoidRootPart.CanCollide = false
-
-    if self.Limbs.Torso and not self.Limbs.Torso:FindFirstChild("NeckJoint") then
-
-        for name: string, data: {CFrame: CFrame, Parent: string} in pairs(AttachmentData) do
-
-            local nAtt: Attachment = self._trove:Add(Instance.new("Attachment"))
-            nAtt.CFrame = data.CFrame
-
-            nAtt.Name = name
-            nAtt.Parent = self.Limbs[data.Parent]
-
-            self.Atts[name] = nAtt
-
-        end
-
-        for name: string, data: { Instance: BallSocketConstraint, Att0: string, Att1: string, Parent: string } in pairs(BallSocketData) do
-
-            local nBS: BallSocketConstraint = self._trove:Add(data.Instance:Clone())
-            nBS.Attachment0 = self.Atts[data.Att0]
-            nBS.Attachment1 = self.Atts[data.Att1]
-
-            nBS.Name = name
-            nBS.Parent = self.Limbs[data.Parent]
-
-        end
-
-    end
-
-    for _, v: BasePart | Motor6D in ipairs(self.Char:GetDescendants()) do
-
-        if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then
-
-            local nCC: NoCollisionConstraint = self._trove:Add(Instance.new("NoCollisionConstraint"))
-
-            nCC.Part0 = v
-            nCC.Part1 = self.Limbs.Torso
-
-            nCC.Name = v.Name
-            nCC.Parent = self.Limbs.Torso
-
-        elseif v:IsA("Motor6D") and v.Name ~= "Root" and v.Name ~= "RootJoint" then
-
-            v.Enabled = false
-
-        end
-
-    end
-
-end
-
---[=[
-    @param forever boolean?
-]=]
-
-function RagdollR6:Stop(forever: boolean?)
-
-    self.Limbs.HumanoidRootPart.CanCollide = true
-
-    self._trove:Destroy()
-
-    for _, v: Motor6D? in ipairs(self.Char:GetDescendants()) do
-
-        if v:IsA("Motor6D") and v.Name ~= "Root" and v.Name ~= "RootJoint" then
-            v.Enabled = true
-        end
-
-    end
-
-    self._trove = not forever and Trove.new() or nil
-
-end
-
---[=[
-
-]=]
 
 function RagdollR6:Checks()
+    if self.Char.Parent == nil then return end
 
-    return self.Player and
-        self.Char and
-        self.Humanoid and
-        self.Char.Humanoid.RigType == Enum.HumanoidRigType.R6
+    return self.Char and self.Humanoid and self.HumanoidRootPart
+end
+
+-- Ragdoll
+function RagdollR6:Start()
+
+    if not self:Checks() then self:Stop(true); return end
+
+    -- self.HumanoidRootPart.CanCollide = false
+
+    self.Humanoid:ChangeState(Enum.HumanoidStateType.Ragdoll)
+
 
 end
 
---[=[
-    Alternative of `RagdollR6:Stop(true)`
-]=]
-
-function RagdollR6:Destroy()
-
-    self:Stop(true)
+-- Stop / Destroy
+function RagdollR6:Stop( forever: boolean? )
 
 end
 
--- RagdollScript/init.server.lua
+++ RagdollScript/init.server.lua
@@ -1,36 +1,15 @@
--- F4YKING - Github
--- 22 September 2023
+-- // F4YKING - GITHUB
+-- // 04 February 2024 - 20:02:32
+
+-- // init.server.lua - RagdollScript.lua
 
 --[[
-    This is an example on how you can use RagdollR6 Module.
+
 ]]
 
 ---- Variables ----
 -- Services
-local Players = game:GetService("Players")
-
--- Ragdoll
-local RagdollR6 = require(script.Parent.RagdollR6)
-
-local Ragdolls = {}
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 ---- Initialize ----
--- PlayerAdded
-Players.PlayerAdded:Connect(function(plr: Player)
 
-    Ragdolls[plr] = RagdollR6.new(plr)
-
-    plr.CharacterAppearanceLoaded:Connect(function(char: Model)
-
-        local hum: Humanoid = char.Humanoid
-
-        Ragdolls[plr]:Init(char)
-
-        hum.Died:Connect(function()
-            Ragdolls[plr]:Start()
-        end)
-
-    end)
-
-
-end)
